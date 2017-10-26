0e3a4e7c7ff5832dff581dbe35a0877565b8b298
Lan Phan <lanc_phan@hotmail.com>
2017-10-26 14:27:46 -0400

    all words are printed with based on their lowercase, greater or lower than 4 characters or both

--- lowercase.rb
+++ lowercase.rb
@@ -1,1 +1,13 @@
-["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+array = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+array.each do |word|
+  if word == word.downcase && word.length > 4
+    puts "#{word}: long and lowercase"
+  elsif  word == word.downcase && word.length <= 4
+    puts "#{word}: lowercase"
+  elsif word.length >= 4
+    puts "#{word}: long"
+  else
+    puts word
+  end
+end
